7e35d10f920b5d529a20cebcee3b67541e8914bd
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2025-03-20 21:06:10 +0300

    backend

---
+++ PlanWise/src/test/java/com/backend/PlanWise/PlanWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.PlanWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
