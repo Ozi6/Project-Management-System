52843fbca73c62b5ca6be691dfddaf196e89f7ec
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2025-04-01 19:23:27 +0300

    Teams Routed

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import ManageRoleModal from "../components/ManageRoleModal";
@@ -117,6 +117,7 @@ const teamsData = [
 ];
 
 const Teams = () => {
+    const { id } = useParams();
   const {t} = useTranslation();
   const [activeTab, setActiveTab] = useState("teams");
   const [teams, setTeams] = useState(teamsData);
@@ -146,49 +147,49 @@ const Teams = () => {
   };
 
   // Custom navigation items for the sidebar - matching ProjectDetails.jsx
-  const customNavItems = [
-    { 
-      id: 'dashboard', 
-      icon: Layout, 
-      label: t("sidebar.dash"), 
-      path: '/dashboard',
-      //color: 'bg-blue-100 text-blue-600',  
-      iconColor: 'text-blue-600',     
-      defaultColor: true
-    },
-    { 
-      id: 'projects', 
-      icon: KanbanSquare, 
-      label: t("sidebar.this"), 
-      path: '/project/1',
-      color: 'bg-purple-100 text-purple-600',
-      iconColor: 'text-purple-600'
-    },
-    { 
-      id: 'activity', 
-      icon: Activity, 
-      label: t("sidebar.act"),
-      path: '/activity',
-      color: 'bg-yellow-100 text-yellow-600',
-      iconColor: 'text-amber-600'
-    },
-    { 
-      id: 'teams', 
-      icon: Users, 
-      label: t("sidebar.team"),
-      path: '/teams',
-      color: 'bg-[var(--sidebar-teams-bg-color)] text-[var(--sidebar-teams-color)]',
-      iconColor: 'text-[var(--sidebar-teams-color)]'
-    },
-    { 
-      id: 'settings', 
-      icon: Settings, 
-      label: t("sidebar.set"),
-      path: '/project/settings',
-      color: 'bg-gray-100 text-gray-600',
-      iconColor: 'text-gray-600'
-    }
-  ];
+    const customNavItems = [
+        {
+            id: 'dashboard',
+            icon: Layout,
+            label: t("sidebar.dash"),
+            path: `/projects`,
+            color: 'bg-[var(--sidebar-dashboard-bg-color)] text-[var(--sidebar-dashboard-color)]',
+            iconColor: 'text-[var(--sidebar-dashboard-color)]',
+            defaultColor: true
+        },
+        {
+            id: 'projects',
+            icon: KanbanSquare,
+            label: t("sidebar.this"),
+            path: `/project/${id}`,
+            color: 'bg-purple-100 text-purple-600',
+            iconColor: 'text-purple-600'
+        },
+        {
+            id: 'activity',
+            icon: Activity,
+            label: t("sidebar.act"),
+            path: `/project/${id}/activity`,
+            color: 'bg-[var(--sidebar-gantt-bg-color)] text-[var(--sidebar-gantt-color)]',
+            iconColor: 'text-[var(--sidebar-gantt-color)]'
+        },
+        {
+            id: 'teams',
+            icon: Users,
+            label: t("sidebar.team"),
+            path: `/project/${id}/teams`,
+            color: 'bg-green-100 text-green-600',
+            iconColor: 'text-green-600'
+        },
+        {
+            id: 'settings',
+            icon: Settings,
+            label: t("sidebar.set"),
+            path: `/project/${id}/settings`,
+            color: 'bg-gray-100 text-gray-600',
+            iconColor: 'text-gray-600'
+        }
+    ];
 
   return (
     <div className="flex flex-col h-screen bg-gray-50">
